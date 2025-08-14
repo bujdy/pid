7ae6c3d2b09ce9434e220220994d07a52d1f16d5
bujdy <adam357bujdos@gmail.com>
2025-08-14 10:36:55 +0200

    fix: localization import

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart' as app_localizations;
+import 'app_localizations.dart' as app_localizations;
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppLocalizations {
